6cc3d6ccab5fb1c689bb801d32bc00ca61c9cd38
IyanaSG <Iyanasogilmore@seattleacademy.org>
2025-05-19 21:48:58 +0000

    project 4 submission

--- Projects/Project4.py
+++ Projects/Project4.py
@@ -31,7 +31,7 @@ y4 = -100
 # TODO - use your own background, and set your four turtles to images of your choice
 set_background("summer")
 t1 = create_sprite("dog2",x1,y1)
-t2 = create_sprite("applecore",x2,y2)
+t2 = create_sprite("apple",x2,y2)
 t3 = create_sprite("kitten",x3,y3)
 t4 = create_sprite("flower",x4,y4)
 
@@ -39,7 +39,8 @@ t4 = create_sprite("flower",x4,y4)
 # # Section 4 - Racing
 # # TODO - set how much each variable changes by and increase the number of repeats to at least 30
 # # TODO - explain here which sprites are faster or slower
-for i in range(100):
+# x2 sprite would probably be the fastest most times because it stays at a consistent high speed the whole time its racing. Whereas The other sprites are either a random speed every time or its just a really low consistent speed.
+for i in range(30):
 	x1 += 8
 	x2 += 15
 	x3 += random.randint (11,17)
@@ -54,11 +55,14 @@ for i in range(100):
 # # Section 5 - Winner
 # # TODO - complete the elif for player 2 winning
 # # TODO - write another elif for player 3 and player 4
-# if x1 >= x2 and x1 >= x3 and x1 >= x4:
-# 	print("player 1 wins!")
-# elif
-# 	print("player 2 wins!")
-
+if x1 >= x2 and x1 >= x3 and x1 >= x4:
+	print("player 1 wins!")
+if x2 >= x1 and x2>= x3 and x2>= x4:
+	print("player 2 wins!")
+if x3 >= x1 and x3 >= x2 and x3 >= x4:
+	print("player 3 wins!")
+if x4 >= x1 and x4 >= x2 and x4 >= x3:
+	print("player 4 wins!")
 
 
 
